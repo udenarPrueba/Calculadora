37b81c533bea7812ac1db79a5aa4a352a13210dc
Johan Sebastian Bravo Muñoz <sebasttian14@gmail.com>
2022-09-18 18:44:46 -0500

    Agregada la funcionalidad de Masa-Sebastian Bravo

--- script.js
+++ script.js
@@ -197,3 +197,12 @@ valor.addEventListener('keyup', () => {
 });
 
 //Función Masa-Sebastian
+let gramos = document.getElementById("resGramos");
+let kilo = document.querySelector('#kilo');
+
+kilo.addEventListener('keyup', () => {
+    let calculo = kilo.value * 1000
+    gramos.value = calculo;
+});
+console.log(kilo);
+console.log(gramos);
